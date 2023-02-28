94d65c10b5bc72bc252453e7ff2d85ad9db75355
cemyasaroguz@gmail.com <cemyasaroguz@gmail.com>
2023-02-28 23:02:16 +0300

    Add project files.

---
+++ RSSVeriCekme/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RSSVeriCekme
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(26, 296);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(251, 34);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "BBC";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(342, 296);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(248, 34);
+			this.button3.TabIndex = 0;
+			this.button3.Text = "HaberTürk";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 19;
+			this.listBox1.Location = new System.Drawing.Point(26, 13);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(564, 270);
+			this.listBox1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(617, 346);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ RSSVeriCekme/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static System.Net.WebRequestMethods;
+
+namespace RSSVeriCekme
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public string rssBbcAkis()
+		{
+			return "https://feeds.bbci.co.uk/turkce/rss.xml";
+		}
+
+		public string rssHabertrukAkis()
+		{
+			return "https://www.haberturk.com/rss/manset.xml";
+		}
+
+		public void rssOku(string gazeteRss)
+		{
+			XmlTextReader xmlOku = new XmlTextReader(gazeteRss);
+			while (xmlOku.Read())
+			{
+				if (xmlOku.Name == "title")
+				{
+					listBox1.Items.Add(xmlOku.ReadString());
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			rssOku(rssBbcAkis());
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			rssOku(rssHabertrukAkis());
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.BackColor = ColorTranslator.FromHtml("0xffeda0");
+		}
+	}
+}
